e2c50955d48d927d269aef5cb354f075b6cc105d
Poutchy <enzo.mauti@etu.univ-smb.fr>
2025-01-31 11:58:41 +0100

    test modification

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-import lib.provaImport as l
+from libraries.paper import Paper
 
-print(l.AffTable)
+p1 = Paper(1, "Test", 10)
+p2 = Paper(1, "Testo", 9)
+p3 = Paper(2, "Testi", 11)
+p4 = Paper(3, "Testa", 10)
+
+print(p1 == p2)
+print(p1 == p3)
+
+print(p1 < p2)
+print(p1 < p3)
+
+print(p1 <= p2)
+print(p1 <= p3)
+print(p1 <= p4)
