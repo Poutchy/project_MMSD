4334c2115e532d9fd5bd0af01cd573d86f35f0eb
Poutchy <enzo.mauti@etu.univ-smb.fr>
2025-03-05 17:56:53 +0100

    modification of outputs

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
-from libraries.functions_algo import initialisation, first_proposition, gain_quota
+from libraries.functions_algo import (first_proposition, gain_quota,
+                                      initialisation, to_json)
 
 # import of all the papers and persons
 
@@ -6,7 +7,9 @@ ConfigsFile = "data/config.json"
 AffFile = "data/2024-02-12-DipInfoAfferenze-PO-PA-RIC-orig.xlsx"
 ProdFile = "data/2024-02-12-prodotti-PO-PA-RIC-02A-03A-03B-04A-04B-2020-instampa.xlsx"
 
-list_persons, nb_persons, list_papers, objectif = initialisation(ConfigsFile, AffFile, ProdFile)
+list_persons, nb_persons, list_papers, objectif = initialisation(
+    ConfigsFile, AffFile, ProdFile
+)
 
 # for paper in list_papers:
 #     print(paper)
@@ -28,16 +31,20 @@ list_persons, nb_proposed_papers = first_proposition(list_persons)
 #     for paper in person.proposed_papers:
 #         print(f"    {paper}")
 
-list_persons, list_papers, nb_proposed_papers = gain_quota(list_persons, list_papers, objectif, nb_proposed_papers)
+list_persons, list_papers, nb_proposed_papers = gain_quota(
+    list_persons, list_papers, objectif, nb_proposed_papers
+)
 
-for person in list_persons:
-    print(f"papers of {person}:")
-    print("  writted paper")
-    for paper in person.writted_papers:
-        print(f"    {paper}")
-    print("  proposed paper")
-    for paper in person.proposed_papers:
-        print(f"    {paper}")
+# sum = 0
+# for person in list_persons:
+#     print(f"papers of {person}:")
+#     print("  writted paper")
+#     for paper in person.writted_papers:
+#         print(f"    {paper}")
+#         sum += paper.value
+#     print("  proposed paper")
+#     for paper in person.proposed_papers:
+#         print(f"    {paper}")
 
 # for person in list_remove:
 #     print(person)
@@ -45,5 +52,9 @@ for person in list_persons:
 #     for paper in person.proposed_papers:
 #         print(f"    {paper}")
 
-print(f"objectif: {objectif}")
-print(f"nb_proposed paper: {nb_proposed_papers}")
+# print(f"objectif: {objectif}")
+# print(f"nb_proposed paper: {nb_proposed_papers}")
+# print(f"sum of values: {sum}")
+
+json = to_json(list_persons)
+print(json)
